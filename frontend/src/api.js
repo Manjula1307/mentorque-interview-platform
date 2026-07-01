d8703a18bcb609ef6bc3d4622ffee113464411e9
unknown <manjulasatapathi97@gmail.com>
2026-07-01 17:29:30 +0530

    point to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://mentorque-interview-platform.onrender.com'
 })
 
 // Automatically attach JWT token to every request
