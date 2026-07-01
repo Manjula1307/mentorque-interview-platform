33225570cdfb959b1836845657f552087848a989
unknown <manjulasatapathi97@gmail.com>
2026-07-01 17:32:33 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const interviewRoutes = require('./routes/interview');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
